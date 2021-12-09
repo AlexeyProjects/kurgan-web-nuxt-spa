6b3bc3dc0ab7d9b36d9a0a0a7746b5206eec2e32
bizin <dmitry.bizin@magenta-technology.com>
2021-12-09 12:49:44 +0400

    Release v1.0.2

--- release.sh
+++ release.sh
@@ -1,1 +1,6 @@
-npm version patch
+npm version patch --no-git-tag-version
+NEW_VERSION=$(node -pe "require('./package.json').version")
+git commit -am "Release v$NEW_VERSION"
+git tag -a "$NEW_VERSION" -m "Release v$NEW_VERSION" HEAD
+git push
+git push --tags
