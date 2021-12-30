190d439f3b8fba5fa927500aad9a9a56fdf76f15
AlexeyProjects <truemanchannel@gmail.com>
2021-12-31 01:28:47 +0400

    try fix modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '~/modules/vueLayers',
+    '~/modules/vuelayers',
   ],
   router: {
     // base: process.env.NODE_ENV === 'production' ? './' : ''
