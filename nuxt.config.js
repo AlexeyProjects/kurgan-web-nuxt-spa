0e6dcb858050dff0c497aa53cf78006c49cd75f6
AlexeyProjects <62065851+AlexeyProjects@users.noreply.github.com>
2022-06-22 00:59:23 +0400

    change title
    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kurganApp',
+    title: 'tour.kurganobl.ru',
     htmlAttrs: {
       lang: 'en'
     },
